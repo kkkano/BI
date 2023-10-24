31821120eaab064e3437eac60aa0a1c071fb61c0
wx <657394554@qq.com>
2023-10-24 23:18:48 +0800

    2023/10/24

---
+++ src/main/java/com/yupi/springbootinit/manager/AiManager.java
@@ -0,0 +1,42 @@
+package com.yupi.springbootinit.manager;
+
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.exception.BusinessException;
+import com.yupi.yucongming.dev.client.YuCongMingClient;
+import com.yupi.yucongming.dev.common.BaseResponse;
+import com.yupi.yucongming.dev.model.DevChatRequest;
+import com.yupi.yucongming.dev.model.DevChatResponse;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * 用于对接 AI 平台
+ */
+@Service
+public class AiManager {
+
+    @Resource
+    private YuCongMingClient yuCongMingClient;
+
+    /**
+     * AI 对话
+     *
+     * @param message
+     * @return
+     */
+    public String doChat(String message) {
+        // 第三步，构造请求参数
+        DevChatRequest devChatRequest = new DevChatRequest();
+        // 模型id，尾后加L，转成long类型
+        devChatRequest.setModelId(1716811466025619457L);
+        devChatRequest.setMessage(message);
+        // 第四步，获取响应结果
+        BaseResponse<DevChatResponse> response = yuCongMingClient.doChat(devChatRequest);
+        // 如果响应为null，就抛出系统异常，提示“AI 响应错误”
+        if (response == null) {
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "AI 响应错误");
+        }
+        return response.getData().getContent();
+    }
+}
---
+++ src/test/java/com/yupi/springbootinit/manager/AiManagerTest.java
@@ -0,0 +1,27 @@
+package com.yupi.springbootinit.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AiManagerTest {
+
+    @Resource
+    private AiManager aiManager;
+
+    @Test
+    void doChat() {
+        String answer = aiManager.doChat("分析需求：\n" +
+                "分析网站用户的增长情况\n" +
+                "原始数据：\n" +
+                "日期，用户数\n" +
+                "1号，10\n" +
+                "2号，20\n" +
+                "3号，30");
+        System.out.println(answer);
+    }
+}
